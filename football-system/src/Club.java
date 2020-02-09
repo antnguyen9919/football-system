2bf7b9d9ad6bbbf8f1d37d7422b8544b60dbbc16
antnguyen9919 <52371079+antnguyen9919@users.noreply.github.com>
2020-02-09 09:53:44 +0100

    Add files via upload
    initial files

---
+++ football-system/src/Club.java
@@ -0,0 +1,12 @@
+public class Club {
+    //có chứa N cầu thủ và 1 Manager, 1 Stadium
+
+    //viết ra các thuộc tính của CLB , vdu: name, city, founded year, values, logo, chứa 1 array Players[], 1 attribute type Manager
+
+
+
+
+
+
+
+}
---
+++ football-system/src/People.java
@@ -0,0 +1,14 @@
+public class People {
+    //class mẹ của Manager và Player
+
+    //tất cả thuộc tính basic của 1 con người, tên, tuổi, ngày sinh,...
+
+
+
+
+
+
+
+
+
+}
