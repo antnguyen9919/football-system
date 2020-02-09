655e8a55875f73bfb112c704f44ae9428b50becf
antnguyen9919 <52371079+antnguyen9919@users.noreply.github.com>
2020-02-09 10:34:36 +0100

    Update Club.java
    change a bit

--- football-system/src/Club.java
+++ football-system/src/Club.java
@@ -2,7 +2,8 @@ public class Club {
     //có chứa N cầu thủ và 1 Manager, 1 Stadium
 
     //viết ra các thuộc tính của CLB , vdu: name, city, founded year, values, logo, chứa 1 array Players[], 1 attribute type Manager
-
+    
+    //another note
 
 
 
