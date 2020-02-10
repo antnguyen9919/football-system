54d390ea5995d2c802382ac155817d3534528d17
antnguyen9919 <52371079+antnguyen9919@users.noreply.github.com>
2020-02-11 00:05:43 +0100

    Update People.java
    added comments

--- football-system/src/People.java
+++ football-system/src/People.java
@@ -9,16 +9,18 @@ public class People {
     private int height;
     private int weight;
     private String nationality;
-    private int experience;
+    private int experience; // tao vẫn chưa hình dung được cái này dùng làm gì
 
     public People(String name, int age, int height, int weight, String nationality, int experience) {
         this.name = name;
-        this.age = age;
-        this.height = height;
-        this.weight = weight;
+        this.age = age;       // phải kiểm soát, lỡ ai đấy input data vô age = 10000 là tiêu, phải đặt điều kiện (16 -> 80), bằng hàm Setter
+        this.height = height; // tương tự, cần có 1 range
+        this.weight = weight;   // tương tự, cần có 1 range
         this.nationality = nationality;
         this.experience = experience;
     }
+    //sắp tới bắt đầu tạo method Setter để kiểm soát input data
+    
 
     public String getName() {
         return name;
